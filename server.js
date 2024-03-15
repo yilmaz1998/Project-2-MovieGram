b841bbd5091e507f87540aa87473ab8335e7c33f
yilmaz1998 <snn.ylmz9804@gmail.com>
2024-03-14 21:15:20 -0400

    Made some changes for port

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+require('dotenv').config()
 const app = express();
 const db = require('./models');
 const methodOverride = require('method-override') 
 const session = require('express-session')
+
+const PORT = process.env.PORT || 3000
 
 const movieController = require('./controllers/movieController');
 const userController = require('./controllers/userController')
@@ -26,6 +29,6 @@ app.use('/session', sessionController)
 
 
 
-app.listen(3000, (req, res) => {
-    console.log('Server is running')
+app.listen(PORT, (req, res) => {
+    console.log('Server is running', PORT)
 })
