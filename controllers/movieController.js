62f33e9e87604d8610d5c36dc2099129aafa319f
yilmaz1998 <snn.ylmz9804@gmail.com>
2024-03-14 20:13:36 -0400

    Final updates

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,18 +3,65 @@ const express = require('express')
 const router = express.Router();
 const isAuth = require('../controllers/isAuth')
 
-// router.use(isAuth)
+router.use(isAuth)
+
+// NEW COMMENT ROUTE
+router.get('/every/:id/comment', (req,res) => {
+    db.Movie.findById(req.params.id, req.body)
+    .then (data => 
+    res.render('newcomment.ejs', {data, currentUser: req.session.currentUser})
+)})
 
 
-// I.N.D.U.C.E.S.
+// MOVIE/ID/COMMENT ROUTE, SHOWS EACH MOVIES' COMMENTS
+router.get('/every/:id/comments',async (req,res) => {
+    await db.Movie.findById(req.params.id, req.body)
+    .populate({
+        path:'comments',
+        populate:{path:'user'}
+    })
+        .then (data => {
+        res.render('comment.ejs', {data, currentUser: req.session.currentUser})
+    })
+})
+
+
+// CREATING COMMENT 
+router.post('/every/:id', async (req, res) => {
+    req.body.user = req.session.currentUser
+    db.Movie.findById(req.params.id, req.body)
+    const newComment = await db.Comment.create(req.body)
+    await db.Movie.findByIdAndUpdate(req.params.id , { $push: { comments: newComment._id} }),
+    res.redirect('/movie/every')
+});
+
+
+// ALL MOVIES SHOW ROUTE
+router.get('/every', (req,res) => {
+    db.Movie.find({}).populate('user')
+    .then (data => {
+        res.render('home.ejs', {data, currentUser: req.session.currentUser})
+    })
+})
+
+
+// ALL MOVIES DETAILS ROUTE
+router.get('/every/:id', (req, res) => {
+    db.Movie.findById(req.params.id).populate('user')
+    .then(data => {
+        res.render('detail.ejs', {data, currentUser:req.session.currentUser})
+    })
+})
+
+
+// I.N.D.U.C.E.S. FOR MOVIES
 
 //INDEX ROUTE
 router.get('/', (req,res) => {
-    // db.Movie.find({user: req.session.currentUser})
-    // .then(data => {
-    //     res.render('index.ejs', {data, currentUser: req.session.currentUser})
-    // })
-    res.send(req.session)
+    db.Movie.find({user: req.session.currentUser})
+    .then(data => {
+        res.render('index.ejs', {data, currentUser: req.session.currentUser})
+    })
 })
 
 
@@ -46,7 +93,7 @@ router.put('/:id', (req,res) => {
 //CREATE ROUTE
 router.post('/', async (req,res) => {
     req.body.user = req.session.currentUser
-    await movie.create(req.body)
+    await db.Movie.create(req.body)
     res.redirect('/movie')
     console.log(req.body)
 })
@@ -69,12 +116,5 @@ router.get('/:id', (req,res) => {
     })
 })
 
-// MOVIE/ID/COMMENT ROUTE, SHOWS EACH MOVIES' COMMENTS
-router.get('/:id/comment', (req,res) => {
-    db.Comment.find({}).populate('user')
-    .then (data => {
-        res.render('comment.ejs', {data, currentUser: req.session.currentUser})
-    })
-})
 
 module.exports = router
